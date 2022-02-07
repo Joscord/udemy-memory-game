c533518e2cd07d9ba676df4eec917308a9034ffc
joscord <josecorderodev@gmail.com>
2022-02-07 14:47:05 -0300

    Added match logic to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import CardGrid from './components/CardGrid';
 
 const cardImages = [
-	{ src: '/assets/images/helmet-1.png' },
-	{ src: '/assets/images/potion-1.png' },
-	{ src: '/assets/images/ring-1.png' },
-	{ src: '/assets/images/scroll-1.png' },
-	{ src: '/assets/images/shield-1.png' },
-	{ src: '/assets/images/sword-1.png' },
+	{ src: '/assets/images/helmet-1.png', matched: false },
+	{ src: '/assets/images/potion-1.png', matched: false },
+	{ src: '/assets/images/ring-1.png', matched: false },
+	{ src: '/assets/images/scroll-1.png', matched: false },
+	{ src: '/assets/images/shield-1.png', matched: false },
+	{ src: '/assets/images/sword-1.png', matched: false },
 ];
 
 function App() {
@@ -27,34 +27,43 @@ function App() {
 		setTurns(0);
 	};
 
-	// Usamos useEffect para hacer la comparación, Cada vez que el estado de una elección cambie se corre la función dentro y si ambos valores son truthy entonces podemos hacer la comparación a través de src. Independiente de cómo resulte la comparación luego se llama a la función de reseteo
 	useEffect(() => {
 		if (choiceOne && choiceTwo) {
 			if (choiceOne.src === choiceTwo.src) {
-				console.log('Cards matched!');
+				// Modificamos el estado de las cartas para cambiar la propiedad matched
+				setCards(prevCards => {
+					return prevCards.map(card => {
+						// Revisamos si la carta del arreglo que estamos iterando es igual a la carta escogida (una o dos da igual porque la propiedad src es la misma). Y si lo es entonces cambiamos la propiedad matched.
+						if (card.src === choiceOne.src) {
+							// Retornamos un nuevo objeto con las mismas propiedades de la carta pero con la propiedad matched cambiada. Así retornamos un nuevo objeto carta al arreglo nuevo (nuevo estado) en lugar de la carta vieja
+							return {
+								...card,
+								matched: true,
+							};
+							// Añadimos una cláusula else para que si las carta del arreglo en la iteración actual no hace match con la opción entonces simplemente devolvamos la misma carta
+						} else {
+							return card;
+						}
+					});
+				});
 			}
 			resetCards();
 		}
 	}, [choiceOne, choiceTwo]);
-	// Definimos la función de reseteo
 	const resetCards = () => {
-		// Reseteamos las cartas
 		setChoiceOne(null);
 		setChoiceTwo(null);
-		// Subimos los turnos
 		setTurns(prevTurn => ++prevTurn);
 	};
 
 	const handleChoice = card => {
 		choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-		// Nótese que no podemos hacer la comparación aquí puesto que los estados no se cambian automáticamente sino que son "agendados" por React
 	};
 
 	return (
 		<div className='app'>
 			<h1>Memory Game</h1>
 			<button onClick={shuffleCards}>New Game</button>
-			{/* Pasamos la función de elección a CardGrip y esta lo pasará a Card */}
 			<CardGrid cards={cards} handleChoice={handleChoice} />
 		</div>
 	);
