cc7628c5a914ffe4aaa704333efe54b55469df47
joscord <josecorderodev@gmail.com>
2022-02-07 14:17:21 -0300

    Defined comparison logic and resetCards function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import CardGrid from './components/CardGrid';
 
@@ -14,7 +14,6 @@ const cardImages = [
 function App() {
 	const [cards, setCards] = useState([]);
 	const [turns, setTurns] = useState(0);
-	// Definimos dos piezas de estado para cuando vayamos a elegir cada carta
 	const [choiceOne, setChoiceOne] = useState(null);
 	const [choiceTwo, setChoiceTwo] = useState(null);
 	const shuffleCards = () => {
@@ -27,12 +26,30 @@ function App() {
 		setCards(shuffledCards);
 		setTurns(0);
 	};
-	// Función para manejar la elección. La función recibe la carta elegida por el usuario. POr ahora sólo la mostraremos en consola.
-	const handleChoice = card => {
-		// Revisamos de cuál elección se trata a través de un operador ternario
-		choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+
+	// Usamos useEffect para hacer la comparación, Cada vez que el estado de una elección cambie se corre la función dentro y si ambos valores son truthy entonces podemos hacer la comparación a través de src. Independiente de cómo resulte la comparación luego se llama a la función de reseteo
+	useEffect(() => {
+		if (choiceOne && choiceTwo) {
+			if (choiceOne.src === choiceTwo.src) {
+				console.log('Cards matched!');
+			}
+			resetCards();
+		}
+	}, [choiceOne, choiceTwo]);
+	// Definimos la función de reseteo
+	const resetCards = () => {
+		// Reseteamos las cartas
+		setChoiceOne(null);
+		setChoiceTwo(null);
+		// Subimos los turnos
+		setTurns(prevTurn => ++prevTurn);
 	};
-	
+
+	const handleChoice = card => {
+		choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+		// Nótese que no podemos hacer la comparación aquí puesto que los estados no se cambian automáticamente sino que son "agendados" por React
+	};
+
 	return (
 		<div className='app'>
 			<h1>Memory Game</h1>
